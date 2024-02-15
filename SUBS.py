a8a5dd309642ceb77da7b033c0d0f5f16c279665
Reinier Kruisbrink <reinier.kruisbrink@sia-partners.com>
2024-02-15 23:18:12 +0100

    change repo name

--- SUBS.py
+++ SUBS.py
@@ -15,7 +15,7 @@ problem = __file__.split('\\')[-1][:-3].lower()
 print(f"problem: {problem}\n")
 
 # Solution
-def task(s):
+def find_subs(s):
     s, t = s.split('\n')
     subs = re.finditer(f'(?=({t}))', s)
     locs = ' '.join([str(x.span()[0]+1) for x in subs])
@@ -24,7 +24,7 @@ def task(s):
 # Sample Test
 sample_data = """GATATATGCATATACTT
 ATAT"""
-sample_output = task(sample_data)
+sample_output = find_subs(sample_data)
 sample_answer = '2 4 10'
 assert sample_output == sample_answer
 print('sample test passed\n')
@@ -34,7 +34,7 @@ data_file = f"data/rosalind_{problem}.txt"
 if os.path.exists(data_file):
     with open(data_file) as f:
         data = '\n'.join([l.strip('\n') for l in f.readlines()])
-    answer = task(data)
+    answer = find_subs(data)
     print(answer, '\n')
 else:
     print('input file not found\n')
